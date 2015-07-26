e13d706fc82419281e92ca2cbb5ce875d60797b3
Liu Xunyu <liuxunyu7@gmail.com>
2015-07-27 00:02:33 +0800

    add flip and delete gravity

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -7,23 +7,66 @@ public class PlayerMoves : MonoBehaviour {
 	public float spring;
 	//public string foreground;
 	private byte jumpState=0;
+
+	int flip = 0;
+	bool collideBridge = false;
+	private Vector3 flipPos, flipPos_brige;
+	private Quaternion qua_upright, qua_reverse;
 	// Use this for initialization
+
+
 	void Start () {
+		qua_upright = new Quaternion (0.0f, 0.0f, 0.0f, 0.0f);
+		qua_reverse = new Quaternion (180.0f, 0.0f, 0.0f, 0.0f);
+
+
+
+
+
+		//rb.gameObject.transform.position = flipPos;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//Vector2 position = (Vector2)transform.position;
 		//transform.position = position + Time.deltaTime*Vector2.right * speed;
-		if(Input.GetKeyDown(KeyCode.Space)&&jumpState<2){
+		if(Input.GetKeyDown(KeyCode.UpArrow)&&jumpState<2){
 			rb.AddForce(new Vector2(0,spring));
 			jumpState++;
+		}
+
+		FlipAction ();
+
+	}
+
+	void FlipAction(){
+		flipPos = rb.gameObject.transform.position;
+		if (Input.GetKey (KeyCode.Space)) {
+			//if player hold the space key, always reverse the gameobject
+			rb.gameObject.transform.localRotation = qua_reverse;
+			if( flip==0 ){
+				flipPos -= new Vector3(0.0f, 2.8f, 0.0f);
+				rb.gameObject.transform.position = flipPos;
+				flip++;
+			}
+		} else if (Input.GetKeyUp (KeyCode.Space)) {
+			rb.gameObject.transform.localRotation = qua_upright;
+
+			flip = 0;
+			flipPos += new Vector3(0.0f, 2.8f, 0.0f);
+			rb.gameObject.transform.position = flipPos;
 		}
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
 		if (other.gameObject.tag.Equals ("Foreground")&&other.relativeVelocity.y<0) {
 			jumpState=0;
+		}
+		if (other.gameObject.tag.Equals ("Bridge") && other.relativeVelocity.y < 0) {
+			collideBridge = true;
+			Debug.Log("detecting collision");
+		} else {
+			collideBridge = false;
 		}
 	}
 }
