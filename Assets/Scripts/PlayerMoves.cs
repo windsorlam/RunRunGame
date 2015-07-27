f2b6d7ba637d4f5f2e6e5674c23d56fe32ac68f9
Liu Xunyu <liuxunyu7@gmail.com>
2015-07-27 11:13:58 +0800

    fix gravity

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -10,18 +10,18 @@ public class PlayerMoves : MonoBehaviour {
 
 	int flip = 0;
 	bool collideBridge = false;
-	private Vector3 flipPos, flipPos_brige;
+	private Vector3 flipPos;
 	private Quaternion qua_upright, qua_reverse;
+
+	Vector2 gra_upright, gra_reverse;
 	// Use this for initialization
 
 
 	void Start () {
 		qua_upright = new Quaternion (0.0f, 0.0f, 0.0f, 0.0f);
 		qua_reverse = new Quaternion (180.0f, 0.0f, 0.0f, 0.0f);
-
-
-
-
+		gra_upright = new Vector2 (0.0f, -9.8f);
+		gra_reverse = new Vector2 (0.0f, 1.0f);
 
 		//rb.gameObject.transform.position = flipPos;
 	}
@@ -36,6 +36,7 @@ public class PlayerMoves : MonoBehaviour {
 		}
 
 		FlipAction ();
+		Debug.Log ("----> Gravity:" + Physics2D.gravity);
 
 	}
 
@@ -44,13 +45,17 @@ public class PlayerMoves : MonoBehaviour {
 		if (Input.GetKey (KeyCode.Space)) {
 			//if player hold the space key, always reverse the gameobject
 			rb.gameObject.transform.localRotation = qua_reverse;
+			Physics2D.gravity = gra_reverse;
+
 			if( flip==0 ){
+				//this function is called in update
 				flipPos -= new Vector3(0.0f, 2.8f, 0.0f);
 				rb.gameObject.transform.position = flipPos;
 				flip++;
 			}
 		} else if (Input.GetKeyUp (KeyCode.Space)) {
 			rb.gameObject.transform.localRotation = qua_upright;
+			Physics2D.gravity = gra_upright;
 
 			flip = 0;
 			flipPos += new Vector3(0.0f, 2.8f, 0.0f);
