a21dc0d08459678e9f6cb1e5a454480104d73367
Liu Xunyu <liuxunyu7@gmail.com>
2015-07-30 22:18:22 +0800

    add highest score

---
+++ Assets/Scripts/SaveHighestScore.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+public class SaveHighestScore : MonoBehaviour {
+	FileStream filestr;
+	StreamWriter writer;
+	string strLine;
+	// Use this for initialization
+	void Start () {
+		filestr = new FileStream("/Users/LIUXUNYU/Desktop/runrungame.txt",FileMode.Open);
+		writer = new StreamWriter(filestr);
+		String highestRecord = Score.highestScore.ToString ();
+
+		try{
+			writer.WriteLine(highestRecord);
+			writer.Close();
+		}
+		catch (IOException ex){
+			Debug.Log(ex.Message);
+			//Console.ReadLine(); //?????
+			return ;
+		}
+		filestr.Close();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -4,16 +4,31 @@ using System.Collections;
 
 public class Score : MonoBehaviour {
 	private float score=0.0f;
+	public static float highestScore;
+	private Text scoreUI;
+	public Text cong;
+	bool highest = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		scoreUI = GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		score += Time.deltaTime*10;
-		Text scoreUI = GetComponent<Text> ();
 		scoreUI.text = ((int)score).ToString();
+
+		if (score >= highestScore && !highest) {
+			cong.gameObject.SetActive(true);
+			highest = true;
+			StartCoroutine(WaitAndPrint(3.0f));
+		}
+	}
+
+	IEnumerator WaitAndPrint(float waitTime){
+		print("----> waiting ... ");
+		yield return new WaitForSeconds (waitTime);
+		cong.gameObject.SetActive (false);
 	}
 }
